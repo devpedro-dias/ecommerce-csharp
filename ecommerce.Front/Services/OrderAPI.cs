25049b24894a251df9a9087ed8e7d442d20edc12
Pedro Dias <pedrocorchogdev@gmail.com>
2025-06-24 10:52:43 -0300

    fea: add table with collapse to Order page

--- ecommerce.Front/Services/OrderAPI.cs
+++ ecommerce.Front/Services/OrderAPI.cs
@@ -1,4 +1,7 @@
-﻿namespace ecommerce.Front.Services;
+﻿using ecommerce.API.DTOs.Response.Order;
+using System.Net.Http.Json;
+
+namespace ecommerce.Front.Services;
 
 public class OrderAPI
 {
@@ -7,5 +10,32 @@ public class OrderAPI
     public OrderAPI(IHttpClientFactory factory)
     {
         _httpClient = factory.CreateClient("API");
+    }
+
+    public async Task<List<OrderResponseDTO>> GetOrdersAsync()
+    {
+        try
+        {
+            var orders = await _httpClient.GetFromJsonAsync<List<OrderResponseDTO>>("api/Order");
+            return orders ?? new List<OrderResponseDTO>();
+        }
+        catch
+        {
+            return new List<OrderResponseDTO>();
+        }
+    }
+
+    public async Task<bool> DeleteOrderAsync(Guid id)
+    {
+        try
+        {
+            var response = await _httpClient.DeleteAsync($"api/Order/{id}");
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error deleting order: {ex.Message}");
+            return false;
+        }
     }
 }
